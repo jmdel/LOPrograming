66f9038f97ac6d5c76083d2d4a237b61cbd87034
jean-michel <jean-michel@delorme.fr>
2016-05-03 10:31:57 +0200

    Add Sleeping

--- src/Main.java
+++ src/Main.java
@@ -11,4 +11,8 @@ public class Main {
 	public static void CiaoWriting(String[] args){
 		System.out.println("Ciao Xavier");
 	}
+	
+	public static void SleepWriting(String[] args){
+		System.out.println("I'm sleeping");
+	}
 }
