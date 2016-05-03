9565f1a3aee2b3bb2f6e081329bfe15e445d660c
jean-michel <jean-michel@delorme.fr>
2016-05-03 10:22:22 +0200

    Add Method helloWriting

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+	}
+	public static void helloWriting(String[] args){
+		System.out.println("Hello Xavier");
 	}
 
 }
