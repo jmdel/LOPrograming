9b2dbc7795a0d71c652834afea4871fe6c999125
jean-michel <jean-michel@delorme.fr>
2016-05-03 10:28:44 +0200

    Add Cio method

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,7 @@ public class Main {
 	public static void helloWriting(String[] args){
 		System.out.println("Hello Xavier");
 	}
-
+	public static void CiaoWriting(String[] args){
+		System.out.println("Ciao Xavier");
+	}
 }
